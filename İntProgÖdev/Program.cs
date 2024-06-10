5c4773f0eb01a5efd7ee2d907994e745dcd2f4be
Lenovo <Lenovo@LAPTOP-58VF5J2K>
2024-06-10 09:54:18 +0300

    .

--- İntProgÖdev/Program.cs
+++ İntProgÖdev/Program.cs
@@ -21,30 +21,3 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-// request --> DNS --> SUNUCU (Server) --> Controller --> Model --> VÝEW
-
-//Controller:Requestleri karþýlarlar.
-
-//IIS: INTERNET INFORMATÝON SERVÝCES:
-//Gelen istekleri karþýlar ve yönlendirmeleri yapar. Javadaki Tomcatin yaptýðý iþi yapar. 
-
-//Render:IIS C# dilinde yazýlanlarý derler ve tarayýcýnýn anlayacaðý dillere dönüþtürür. Buna Render denir.
-
-//Razor: Html ve C# birlikte çalýþmasý
-
-//Ctrl+M+G : Models ve view arasýnda gidip gelmemizi saðlayan kýsayol tuþlarý.
-
-//Controller --> view Veri taþýmada
-//--- ViewData: Bir koleksiyon yapýsýdýr. ViewData bir dictionary yapýsýdýr. Anahtar ve deðer çiftlerinden oluþur.
-//--- ViewBag:  Arka planda viewData koleksiyonunu kullanan, dynamic yapýda bir veri taþýma yapýsýdýr. Dynamic yapýlar sadece çalýþma zamanýnda hata verirler.
-//--- ViewModel:
-
-//ORM: Object Relational Mapping
-//Entity FrameWork First
-
-//Entity ->
-//Modal Class ->
-//DBContext->DB iþlemleri için gerekli class
-//DBSet -> DB'deki tablolarýn ram'deki karþýlýðý. Generic yapýdýr.
-
-//Modal -> DBSet -> SaveChanges(); -> DB
