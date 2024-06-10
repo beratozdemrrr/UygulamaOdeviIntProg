a97053d3106e01df8acaf80d1e5f6bf26525a78c
Lenovo <Lenovo@LAPTOP-58VF5J2K>
2024-06-10 09:51:13 +0300

    Add project files.

---
+++ EntityAppProject/OkulDBContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+
+namespace EntityAppProject
+{
+    public class OkulDBContext : DbContext
+    {
+        public DbSet<Ogrenci> Ogrenciler { get; set; }
+        public DbSet<Ders> Dersler { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-58VF5J2K\SQLEXPRESS;Integrated Security=true;Initial Catalog=OkulDBEF2;TrustServerCertificate=true");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ogrenci>().ToTable("tblOgrenciler");
+            modelBuilder.Entity<Ogrenci>().Property(o => o.Ad).HasColumnType("varchar").HasMaxLength(30).IsRequired();
+            modelBuilder.Entity<Ogrenci>().Property(o => o.Soyad).HasColumnType("varchar").HasMaxLength(30).IsRequired();
+            modelBuilder.Entity<Ogrenci>().Property(o => o.Numara).IsRequired();
+        }
+    }
+}
+
+
+//Fluent Api
---
+++ İntProgÖdev/Controllers/OgretmenController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace İntProgÖdev.Controllers
+{
+    public class OgretmenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult OgretmenListe()
+        {
+            return View();
+        }
+    }
+}
